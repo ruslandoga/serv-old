b404ad5c64b348033d77aa14d9ce0646ebe77c58
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-01-27 00:13:06 +0700

    init

---
+++ lib/serv.ex
@@ -0,0 +1,18 @@
+defmodule Serv do
+  @moduledoc """
+  Documentation for `Serv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Serv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
