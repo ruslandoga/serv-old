ef974e10367a52e8faf3887050660fed55a24213
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-02-01 15:06:28 +0700

    tests

--- test/serv_test.exs
+++ test/serv_test.exs
@@ -1,5 +1,5 @@
 defmodule ServTest do
-  use ExUnit.Case
+  use Serv.ConnCase, async: true
 
   defmodule OkPlug do
     @behaviour Plug
@@ -13,37 +13,23 @@ defmodule ServTest do
     end
   end
 
-  test "it works" do
-    serv = start_supervised!({Serv, plug: OkPlug})
-    %{addr: addr, port: port} = Serv.sockname(serv)
-
-    {:ok, conn} = Mint.HTTP.connect(:http, addr, port, mode: :passive, hostname: "localhost")
-    {:ok, conn, _ref} = Mint.HTTP.request(conn, "GET", "/", [], "")
-
-    assert [200, headers | data] = recv(conn)
-
-    assert headers == [
-             {"content-length", "3"},
-             {"cache-control", "max-age=0, private, must-revalidate"}
-           ]
-
-    assert IO.iodata_to_binary(data) == "Ok."
-  end
-
-  defp recv(conn, acc \\ []) do
-    with {:ok, conn, messages} <- Mint.HTTP.recv(conn, 0, :infinity) do
-      case handle_messages(messages, []) do
-        {:ok, messages} -> acc ++ messages
-        {:more, messages} -> recv(conn, acc ++ messages)
-      end
+  describe "basic" do
+    setup do
+      {:ok, plug: OkPlug}
     end
-  end
 
-  defp handle_messages([{k, _ref, v} | rest], acc) when k in [:status, :headers, :data] do
-    handle_messages(rest, [v | acc])
-  end
+    setup [:started, :connected]
 
-  defp handle_messages([{:done, _ref}], acc) do
-    {:ok, :lists.reverse(acc)}
+    test "it works", %{conn: conn} do
+      {:ok, conn, _ref} = request(conn, "GET", "/")
+      assert [200, headers | data] = recv(conn)
+
+      assert headers == [
+               {"content-length", "3"},
+               {"cache-control", "max-age=0, private, must-revalidate"}
+             ]
+
+      assert IO.iodata_to_binary(data) == "Ok."
+    end
   end
 end
---
+++ test/support/conn_case.ex
@@ -0,0 +1,52 @@
+defmodule Serv.ConnCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Plug.Conn
+      import Serv.ConnCase
+    end
+  end
+
+  def connect(port, opts \\ []) do
+    addr = opts[:addr] || {127, 0, 0, 1}
+    hostname = opts[:hostname] || "localhost"
+    Mint.HTTP.connect(:http, addr, port, mode: :passive, hostname: hostname)
+  end
+
+  def request(conn, verb, path, headers \\ [], body \\ "") do
+    Mint.HTTP.request(conn, verb, path, headers, body)
+  end
+
+  def recv(conn, acc \\ []) do
+    with {:ok, conn, messages} <- Mint.HTTP.recv(conn, 0, :infinity) do
+      case handle_messages(messages, []) do
+        {:ok, messages} -> acc ++ messages
+        {:more, messages} -> recv(conn, acc ++ messages)
+      end
+    end
+  end
+
+  def handle_messages([{k, _ref, v} | rest], acc) when k in [:status, :headers, :data] do
+    handle_messages(rest, [v | acc])
+  end
+
+  def handle_messages([{:done, _ref}], acc) do
+    {:ok, :lists.reverse(acc)}
+  end
+
+  def handle_messages([], acc) do
+    {:more, acc}
+  end
+
+  def started(ctx) do
+    plug = ctx[:plug] || raise "missing :plug"
+    {:ok, serv: start_supervised!({Serv, plug: plug})}
+  end
+
+  def connected(%{serv: serv}) do
+    %{port: port} = Serv.sockname(serv)
+    {:ok, conn} = connect(port)
+    {:ok, conn: conn}
+  end
+end
